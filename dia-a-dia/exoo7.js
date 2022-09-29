ce4d15480f045eea6f4de352c5b2960258489495
Lucas Leite <lucasleite14077@gmail.com>
2022-09-29 14:16:14 -0300

    subindo tudo

---
+++ dia-a-dia/exoo7.js
@@ -0,0 +1,12 @@
+console.log('o console funcionol corretamente')
+
+var vel = 60.5
+
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if(vel> 50){
+
+    console.log('você ultrapassou o limite da via, e infelimente foi multado')
+}
+    else{
+console.log(`cuidado com a velocidade, não ponha vidas em risco...`)
+    }
